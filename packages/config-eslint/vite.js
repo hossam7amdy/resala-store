093ef00851e787a27d7d5d0fa4f5346dfc303fe7
Hossam Hamdy <hossamhamdy117@gmail.com>
2025-09-13 16:33:41 +0300

    feat: add initial admin dashboard fork

--- packages/config-eslint/vite.js
+++ packages/config-eslint/vite.js
@@ -1,10 +1,11 @@
 import js from '@eslint/js'
+import { defineConfig } from '@eslint/config-helpers'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config({
+export const config = defineConfig({
   extends: [js.configs.recommended, ...tseslint.configs.recommended],
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
